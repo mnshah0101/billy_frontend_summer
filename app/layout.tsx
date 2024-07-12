82b8fc36cf79e4aed6c337a0d076a7bdda0e21eb
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-07-11 22:16:10 -0400

    previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
 
         <meta name="description" content="Your favorite betting assistant." />
 
-        <meta name="keywords" content="betting, assistant, sports betting, Billy Bets, betting app" />
+        <meta
+          name="keywords"
+          content="betting, assistant, sports betting, Billy Bets, betting app"
+        />
 
         <meta name="robots" content="index, follow" />
 
@@ -36,12 +39,14 @@ export default function RootLayout({
 
         <link rel="icon" href="/favicon.ico" />
 
-       
-
-
-       
-
-
+        <meta property="og:image" content="/preview.png" />
+        <meta property="og:title" content="Billy Bets" />
+        <meta
+          property="og:description"
+          content="Your favorite betting assistant."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://askbilly.bet" />
       </head>
 
       <body className={inter.className}>{children}</body>
